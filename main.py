a14455dfb5351cfe497fcec5e5245843a2129c1b
Ch1ps-dot <2241180437@qq.com>
2026-01-06 21:45:40 +0800

    debug

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from voltron.fuzz import Fuzzer
 def test_lightftp():
     fuzzer = Fuzzer(
         target_name='lightftp',
-        time_limit=10
+        time_limit_min=10
     )
     fuzzer.fuzz(
         algo='rand'
