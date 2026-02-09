9104ed5b2241ffca0f919910b7cc7c5c9cb009de
Ch1ps-dot <2241180437@qq.com>
2026-02-09 21:52:37 +0800

    exit

--- voltron/scheduler/havoc.py
+++ voltron/scheduler/havoc.py
@@ -164,7 +164,7 @@ class Havoc:
         seq = []
         if len(cons.res_seq) > self.max_seq_len:
             for i in range(len(cons.res_seq)):
-                if cons.req_seq[j] != '-':
+                if cons.req_seq[i] != '-':
                     seq.append((cons.req_seq[i], cons.content[i][0]))
             self.useful_seq.append(seq)
         
