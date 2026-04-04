fdf60d2776568b01b2a439aae71e26fb38356a74
pfqiu <220245470@seu.edu.cn>
2026-04-04 15:15:47 +0800

    debug

--- configs/forked-daapd/post.sh
+++ configs/forked-daapd/post.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 #Network deamons needed by forked-daapd
-sudo /etc/init.d/dbus start
-sudo /etc/init.d/avahi-daemon start
+# sudo /etc/init.d/dbus start
+# sudo /etc/init.d/avahi-daemon start
 
-sudo /etc/init.d/dbus status
-if [ $? -ne 0 ]
-then
-  echo "Unable to run DBUS"
-  exit 1
-fi
+# sudo /etc/init.d/dbus status
+# if [ $? -ne 0 ]
+# then
+#   echo "Unable to run DBUS"
+#   exit 1
+# fi
 
-sudo /etc/init.d/avahi-daemon status
-if [ $? -ne 0 ]
-then
-  echo "Unable to run AVAHI daemon"
-  exit 1
-fi
+# sudo /etc/init.d/avahi-daemon status
+# if [ $? -ne 0 ]
+# then
+#   echo "Unable to run AVAHI daemon"
+#   exit 1
+# fi
