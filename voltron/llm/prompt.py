4d1ae486066b96523ea7f0b5c31017951a4265e7
pfqiu <220245470@seu.edu.cn>
2026-04-08 13:07:48 +0800

    debug: prompt error

--- voltron/llm/prompt.py
+++ voltron/llm/prompt.py
@@ -15,55 +15,55 @@ class Prompter:
             dir.mkdir()
         
         try:
-            self._path_gen_generator = dir / "build" /"generator_generation.md"
+            self._path_gen_generator = dir / "builder" /"generator_generation.md"
             with self._path_gen_generator.open('r+') as f:
                 self._tem_gen_generator = Template(f.read())
                     
-            self._path_gen_parser = dir / "build" / "parser_generation.md"
+            self._path_gen_parser = dir / "builder" / "parser_generation.md"
             with self._path_gen_parser.open('r+') as f:
                 self._tem_gen_parser = Template(f.read())
                 
-            self._path_res_query = dir / "build" / "response_query.md"
+            self._path_res_query = dir / "builder" / "response_query.md"
             with self._path_res_query.open('r+') as f:
                 self._tem_res_query = Template(f.read())
             
-            self._path_req_query = dir / "build" / "request_query.md"
+            self._path_req_query = dir / "builder" / "request_query.md"
             with self._path_req_query.open('r+') as f:
                 self._tem_req_query = Template(f.read())
                 
-            self._path_doc_analyze = dir / "build" / "doc_analyze.md" 
+            self._path_doc_analyze = dir / "builder" / "doc_analyze.md" 
             with self._path_doc_analyze.open('r+') as f:
                 self._tem_doc_analyze = Template(f.read())
             
-            self._path_ir_generation = dir / "build" / "ir_generation.md"
+            self._path_ir_generation = dir / "builder" / "ir_generation.md"
             with self._path_ir_generation.open('r+') as f:
                 self._tem_ir_generation = Template(f.read())
             
-            self._path_ir_repair = dir / "build" / "ir_repair.md"
+            self._path_ir_repair = dir / "builder" / "ir_repair.md"
             with self._path_ir_repair.open('r+') as f:
                 self._tem_ir_repair = Template(f.read())
             
-            self._path_initial_symbols = dir / "build" / "initial_symbols.md"
+            self._path_initial_symbols = dir / "builder" / "initial_symbols.md"
             with self._path_initial_symbols.open('r+') as f:
                 self._tem_initial_symbols = Template(f.read())
             
-            self._path_possible_response = dir / "build" / "possible_response.md"
+            self._path_possible_response = dir / "builder" / "possible_response.md"
             with self._path_possible_response.open('r+') as f:
                 self._tem_possible_response = Template(f.read())
             
-            self._path_infer_dependency = dir / "build" / "infer_dependency.md"
+            self._path_infer_dependency = dir / "builder" / "infer_dependency.md"
             with self._path_infer_dependency.open('r+') as f:
                 self._tem_infer_dependency = Template(f.read())
             
-            self._path_evolve_generator = dir / "evolve" / "generator_evolve.md"
+            self._path_evolve_generator = dir / "evolver" / "generator_evolve.md"
             with self._path_evolve_generator.open('r+') as f:
                 self._tem_generator_evolve = Template(f.read())
                 
-            self._path_evolve_parser = dir / "evolve" / "parser_evolve.md"
+            self._path_evolve_parser = dir / "evolver" / "parser_evolve.md"
             with self._path_evolve_parser.open('r+') as f:
                 self._tem_parser_evolve = Template(f.read())
                 
-            self._path_mutator_evolve = dir / "evolve" / 'generator_mutate.md'
+            self._path_mutator_evolve = dir / "evolver" / 'generator_mutate.md'
             with self._path_mutator_evolve.open('r+') as f:
                 self._tem_mutator_evolve = Template(f.read())
                 
